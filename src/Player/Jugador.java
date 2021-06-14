f65e634e315f0beae5bd743be2e171e43192028c
Orantoon <71191565+Orantoon@users.noreply.github.com>
2021-06-13 22:35:37 -0600

    Clase Jugador

---
+++ src/Player/Jugador.java
@@ -0,0 +1,15 @@
+package Player;
+
+public class Jugador {
+    public String nickname;
+    public String inscripcion;
+    public float creditos;
+    //public Ticket ticket;
+
+    public Jugador(){
+        nickname = " ";
+        inscripcion = " ";
+        creditos = (float) 1000.00;
+        //ticket = null;
+    }
+}
